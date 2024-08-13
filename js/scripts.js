7f8484f4eea9648ca82cad6e8b5db15934437121
Joe K <61050711+JKoss2@users.noreply.github.com>
2024-08-13 01:59:36 -0500

    Added content to FAQs. Styled venue pictures.

--- js/scripts.js
+++ js/scripts.js
@@ -200,12 +200,10 @@ $(document).ready(function () {
     var acc = document.getElementsByClassName("accordion");
     var i;
 
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-
     for (i = 0; i < acc.length; i++) {
         acc[i].addEventListener("click", function() {
             this.classList.toggle("active");
+            this.classList.toggle("BR-OR");
             var panel = this.nextElementSibling;
             if (panel.style.maxHeight) {
                 panel.style.maxHeight = null;
